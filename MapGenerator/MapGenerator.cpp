b8eea116c8fa37572e02a361fde5fd481887761c
ExtremelyAmateurWebDeveloper <lemailbusiness171@gmail.com>
2026-02-28 23:58:01 +0200

    The great optimization of 2026. Int datatype massacre.

--- MapGenerator/MapGenerator.cpp
+++ MapGenerator/MapGenerator.cpp
@@ -11,49 +11,49 @@ const int cellsize = 3;
 
 class coordinate {
 public:
-    int x;
-    int y;
-    coordinate(int coordx, int coordy) : x(coordx), y(coordy) {}
+    __int16 x;
+    __int16 y;
+    coordinate(__int16 coordx, __int16 coordy) : x(coordx), y(coordy) {}
 };
 
 class PlacedRoom {
 public:
-    int roomId;
+    unsigned __int16 roomId;
     coordinate coord;
-    int rotation;
+    unsigned __int8 rotation;
 
 
-    PlacedRoom(int id, coordinate c, int rot) : roomId(id), coord(c.x, c.y), rotation(rot) {}
+    PlacedRoom(unsigned __int16 id, coordinate c, unsigned __int16 rot) : roomId(id), coord(c.x, c.y), rotation(rot) {}
 };
 
 
 class twoarray {
 private:
-    std::vector<int> array;
+    std::vector<unsigned __int8> array;
 
 public:
-    int size;
-    twoarray(int height) {
+    unsigned __int8 size;
+    twoarray(unsigned __int8 height) {
         size = height;
         array.reserve(size * size);
-        for (int i = 0; i < size * size; i++) {
+        for (unsigned __int16 i = 0; i < size * size; i++) {
             array.push_back(0);
         }
     }
     twoarray() {
         size = cellsize;
         array.reserve(size * size);
-        for (int i = 0; i < size * size; i++) {
+        for (unsigned __int8 i = 0; i < size * size; i++) {
             array.push_back(0);
         }
     }
     //returns -1 if outside array size
-    int get(coordinate coord) {
+    unsigned __int8 get(coordinate coord) {
         if (coord.x >= size || coord.y >= size || coord.x < 0 || coord.y < 0) return -1;
         return array[size * coord.x + coord.y];
     }
     //returns false if outside array size
-    bool set(coordinate coord, int input) {
+    bool set(coordinate coord, __int8 input) {
         if (coord.x >= size || coord.y >= size || coord.x < 0 || coord.y < 0) return false;
         array[size * coord.x + coord.y] = input;
         return true;
@@ -67,16 +67,16 @@ public:
 //(1,1),(3,1) be defined as (wall on the top and left, wall on the top and right), (door on the left wall on the bottom, wall on the right and bottom)
 
 
-twoarray roomset(std::vector<int> row1, std::vector<int> row2, std::vector<int> row3) {
+twoarray roomset(std::vector<unsigned __int8> row1, std::vector<unsigned __int8> row2, std::vector<unsigned __int8> row3) {
     twoarray temp{};
 
-    for (int i = 0; i < row1.size(); i++) {
+    for (unsigned __int8 i = 0; i < row1.size(); i++) {
         temp.set(coordinate(0, i), row1[i]);
     }
-    for (int i = 0; i < row2.size(); i++) {
+    for (unsigned __int8 i = 0; i < row2.size(); i++) {
         temp.set(coordinate(1, i), row2[i]);
     }
-    for (int i = 0; i < row3.size(); i++) {
+    for (unsigned __int8 i = 0; i < row3.size(); i++) {
         temp.set(coordinate(2, i), row3[i]);
     }
 
@@ -85,8 +85,8 @@ twoarray roomset(std::vector<int> row1, std::vector<int> row2, std::vector<int> 
 
 
 void place(twoarray& room, twoarray& map, coordinate coord) {
-    for (int x = 0; x < room.size; x++) {
-        for (int y = 0; y < room.size; y++) {
+    for (unsigned __int8 x = 0; x < room.size; x++) {
+        for (unsigned __int8 y = 0; y < room.size; y++) {
             if (room.get(coordinate(x, y)) != 0) {
                 map.set(coordinate(x + coord.x, y + coord.y), room.get(coordinate(x, y)));
             }
@@ -96,22 +96,22 @@ void place(twoarray& room, twoarray& map, coordinate coord) {
 }
 
 bool placeablecheck(twoarray& room, twoarray& map, coordinate& coord) {
-    int correctdoorcounter = 0;
+    unsigned __int8 correctdoorcounter = 0;
 
-    for (int x = 0; x < room.size; x++) {
-        for (int y = 0; y < room.size; y++) {
-            int mapX = x + coord.x;
-            int mapY = y + coord.y;
+    for (unsigned __int8 x = 0; x < room.size; x++) {
+        for (unsigned __int8 y = 0; y < room.size; y++) {
+            unsigned __int8 mapX = x + coord.x;
+            unsigned __int8 mapY = y + coord.y;
 
-            int roomVal = room.get(coordinate(x, y));
-            int mapVal = map.get(coordinate(mapX, mapY));
+            unsigned __int8 roomVal = room.get(coordinate(x, y));
+            unsigned __int8 mapVal = map.get(coordinate(mapX, mapY));
 
             if (mapVal != 0 && roomVal != 0) {
                 return false;
             }
 
             switch (roomVal) {
-            case 2: 
+            case 2:
                 if (map.get(coordinate(mapX, mapY + 1)) == 4) correctdoorcounter++;
                 break;
             case 3:
@@ -135,9 +135,9 @@ bool placeablecheck(twoarray& room, twoarray& map, coordinate& coord) {
 }
 
 void printtwoarray(twoarray room) {
-    for (int x = 0; x < room.size; x++) {
+    for (unsigned __int8 x = 0; x < room.size; x++) {
         std::string print = "";
-        for (int y = 0; y < room.size; y++) {
+        for (unsigned __int8 y = 0; y < room.size; y++) {
             print.append(std::to_string(room.get(coordinate(x, y))));
         }
         std::cout << print << std::endl;
@@ -146,19 +146,19 @@ void printtwoarray(twoarray room) {
 
 twoarray rotate(twoarray room) {
     twoarray temp = twoarray();
-    for (int x = 0; x < room.size; x++) {
-        for (int y = 0; y < room.size; y++) {
+    for (unsigned __int8 x = 0; x < room.size; x++) {
+        for (unsigned __int8 y = 0; y < room.size; y++) {
             temp.set(coordinate(y, x), room.get(coordinate(x, y)));
         }
     }
     twoarray temp2 = temp;
-    for (int x = 0; x < room.size; x++) {
-        for (int y = 0; y < room.size; y++) {
+    for (unsigned __int8 x = 0; x < room.size; x++) {
+        for (unsigned __int8 y = 0; y < room.size; y++) {
             temp.set(coordinate(x, y), temp2.get(coordinate(x, temp2.size - y - 1)));
         }
     }
-    for (int x = 0; x < room.size; x++) {
-        for (int y = 0; y < room.size; y++) {
+    for (unsigned __int8 x = 0; x < room.size; x++) {
+        for (unsigned __int8 y = 0; y < room.size; y++) {
             int val = temp.get(coordinate(x, y));
 
             if (val == 2) {
@@ -178,18 +178,18 @@ twoarray rotate(twoarray room) {
     return temp;
 }
 
-twoarray rotate(twoarray room, int times) {
+twoarray rotate(twoarray room, unsigned __int8 times) {
     twoarray temp = room;
-    for (int i = 0; i < times; i++) {
+    for (unsigned __int8 i = 0; i < times; i++) {
         temp = rotate(temp);
     }
     return temp;
 }
 
 bool fullPlaceAlgorithm(int roomId, twoarray& room, twoarray& map, coordinate coord, std::vector<PlacedRoom>& trackingList) {
-    for (int x = 0; x < 3; x++) {
-        for (int y = 0; y < 3; y++) {
-            for (int r = 0; r < 4; r++) {
+    for (unsigned __int8 x = 0; x < 3; x++) {
+        for (unsigned __int8 y = 0; y < 3; y++) {
+            for (unsigned __int8 r = 0; r < 4; r++) {
                 coordinate newCoord = coordinate(coord.x + x - 1, coord.y + y - 1);
                 twoarray currentRoom = rotate(room, r);
 
@@ -238,8 +238,8 @@ int main()
     std::mt19937 g(rd());
 
     std::vector<coordinate> allCoords;
-    for (int x = 0; x < map.size; x++) {
-        for (int y = 0; y < map.size; y++) {
+    for (unsigned __int16 x = 0; x < map.size; x++) {
+        for (unsigned __int16 y = 0; y < map.size; y++) {
             allCoords.push_back(coordinate(x, y));
         }
     }
@@ -251,9 +251,9 @@ int main()
         std::shuffle(allCoords.begin(), allCoords.end(), g);
 
         for (coordinate currentCoord : allCoords) {
-            int startRandom = rand() % rooms.size();
-            for (int i = 0; i < rooms.size(); i++) {
-                int r = (startRandom + i) % rooms.size();
+            unsigned __int16 startRandom = rand() % rooms.size();
+            for (unsigned __int16 i = 0; i < rooms.size(); i++) {
+                unsigned __int16 r = (startRandom + i) % rooms.size();
 
                 if (fullPlaceAlgorithm(r, rooms[r], map, currentCoord, placedRoomsList)) {
                     mapChanged = true;
@@ -268,7 +268,7 @@ int main()
 
     std::cout << "\n--- Generation Complete ---\n";
     std::cout << "Total Rooms Placed: " << placedRoomsList.size() << "\n\n";
-    for (int i = 0; i < placedRoomsList.size(); i++) {
+    for (unsigned __int16 i = 0; i < placedRoomsList.size(); i++) {
         std::cout << "Room [" << placedRoomsList[i].roomId << "] "
             << "at (" << placedRoomsList[i].coord.x << ", " << placedRoomsList[i].coord.y << ") "
             << "Rotated " << placedRoomsList[i].rotation * 90 << " degrees.\n";
@@ -276,4 +276,3 @@ int main()
 
 
 }
-
